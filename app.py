afc0314035815129ecb08c122d11bb434af01cd5
K Mahesh <mahesh0352002@gmail.com>
2023-05-19 14:43:56 +0530

    commit

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask,render_template
-
+from flask_mysqldb import MySQL
 app=Flask(__name__)
+
+app.config["MYSQL_HOST"]="localhost"
+app.config["MYSQL_USER"]="root"
+app.config["MYSQL_PASSWORD"]="123456"
+app.config["MYSQL_DB"]="crud"
+app.config["MYSQL_CURSORCLASS"]="DictCursor"
+mysql=MySQL(app)
+
 
 @app.route("/")
 def home():
-   return render_template("home.html") #render_template class is used to load html from another folder
+  con= mysql.connection.cursor()
+  sql="select * from users"
+  con.execute(sql)
+  res=con.fetchall()
+  return render_template("home.html",datas=res) #render_template class is used to load html from another folder
 
 if(__name__== '__main__'):
     app.run(debug=True)
