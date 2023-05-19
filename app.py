45f47bc838561fb650405101e9153110d0ba3934
K Mahesh <mahesh0352002@gmail.com>
2023-05-19 17:26:52 +0530

    update query commit

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ def home():
   sql="select * from users"
   con.execute(sql)
   res=con.fetchall()
-  return render_template("home.html",datas=res) #render_template class 
-
+  return render_template("home.html",datas=res) #render_template class
+  
 #New User
 @app.route("/addUsers",methods=['GET','POST'])
 def addUsers():
@@ -28,12 +28,32 @@ def addUsers():
        age=request.form['age']
        con= mysql.connection.cursor()
        sql="insert into users (NAME,CITY,AGE) values(%s,%s,%s)"
-       con.execute(sql,[name,city,age])
+       con.execute(sql,[name,city,age]) 
        mysql.connection.commit()
        con.close()
        return redirect(url_for("home"))
     return render_template("addUsers.html")
-   
+
+#Update User
+@app.route("/editUser/<string:id>",methods=['GET','POST'])    
+def editUser(id):
+    con=mysql.connection.cursor()
+    if request.method=='POST':
+        name=request.form['name']
+        city=request.form['city']
+        age=request.form['age']
+        sql="update users set NAME=%s,CITY=%s,AGE=%s where ID=%s"
+        con.execute(sql,[name,city,age,id])
+        mysql.connection.commit()
+        con.close()
+        return redirect(url_for("home"))
+        
+       
+    sql="select * from users where ID=%s"
+    con.execute(sql,[id])
+    res=con.fetchone()
+    return render_template("editUsers.html",datas=res)
+  
   
 if(__name__== '__main__'):
     app.run(debug=True)
